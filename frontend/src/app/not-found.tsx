af8f96052877ff8618ab9185f1446f689bd413a9
Diulgher Artiom <42372666+GeneralTao2@users.noreply.github.com>
2025-09-02 22:39:52 +0300

    Fix client directive and lint issues

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="not-found-container">
       <h1>404</h1>
-      <p>We couldn't find that page.</p>
+      <p>We couldn&apos;t find that page.</p>
       <Link href="/" className="button">
         Go home
       </Link>
